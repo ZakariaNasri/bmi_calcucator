2aae139aa65e98c0448adf756911832f9914ee07
ZakariaNasri <zakaria.nasri@univconstantine2.dz>
2024-02-10 08:56:25 +0000

    Specifying dark mode with some new stuff

---
+++ lib/input_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({super.key});
+
+  @override
+  State<InputPage> createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'BMI CALCULATOR',
+          ),
+        ),
+      ),
+      body: Center(child: Text('Bmicalc')),
+    );
+  }
+}
