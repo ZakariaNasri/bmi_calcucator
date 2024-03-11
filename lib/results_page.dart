0abf67f04e585576fa6af84f5cd82595930711c8
ZakariaNasri <zakaria.nasri@univ-constantine2.dz>
2024-03-11 22:18:18 +0000

    fixing results page

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 import 'reusable_card.dart';
+import 'constants.dart';
 
 class ResultsPage extends StatelessWidget {
   const ResultsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MyCard(
-      bgColor: Colors.white,
-      childWidget: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [Text('Result')],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bmi Calculator'),
+      ),
+      body: MyCard(
+        bgColor: kActiveCardBG,
+        childWidget: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Text('Result')],
+        ),
       ),
     );
   }
